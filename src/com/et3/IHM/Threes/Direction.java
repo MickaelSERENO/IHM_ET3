05db5f39d054939733c297ce59bd504faca4ee3e
Gaulois94 <serenomickael@gmail.com>
2016-05-07 03:06:31 +0200

    Directon class (Top, Bottom, Left, Right)

---
+++ src/com/et3/IHM/Threes/Direction.java
@@ -0,0 +1,9 @@
+package com.et3.IHM.Threes;
+
+public class Direction
+{
+	public final static int TOP    = 0;
+	public final static int BOTTOM = 1;
+	public final static int LEFT   = 2;
+	public final static int RIGHT  = 3;
+}
